a02cf25350663238bdcc9c9cf116445977ccd295
vc-souza <vinicius.cp.souza@gmail.com>
2024-07-20 22:57:44 -0300

    Working MySQL database

---
+++ db/init/1_inserts.sql
@@ -0,0 +1,15 @@
+INSERT INTO feeds (
+    title,
+    url
+) VALUES (
+    'DeepMind',
+    'https://deepmind.com/blog/feed/basic/'
+),
+(
+    'O\’Reilly',
+    'http://feeds.feedburner.com/oreilly/radar/atom'
+),
+(
+    'Stack Overflow',
+    'http://blog.stackoverflow.com/feed/'
+);
